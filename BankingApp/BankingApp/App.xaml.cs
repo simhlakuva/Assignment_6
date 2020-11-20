4425ee8897c8e98d3cc08b935918213135b1e912
Simamkele hlakuva <shlakuva@my365-uwc.ac.za>
2020-11-20 19:51:58 +0200

    Add project files.

---
+++ BankingApp/BankingApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BankingApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected  void DMoneyButton()
+        {
+            
+            
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ BankingApp/BankingApp/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Banking;
+
+namespace BankingAppSH
+{
+    public partial class MainPage : ContentPage
+    {
+        private BankAccount _account;
+        public MainPage()
+        {
+
+            InitializeComponent();
+            Bank fnb = new Bank("First National Bank", 4324, "Kenilworth");
+
+            Customer myNewCustomer = new Customer("7766445424", "10 me at home", "Bob", "The Builder");
+
+            fnb.AddCustomer(myNewCustomer);
+
+
+
+            var account = myNewCustomer.ApplyForBankAccount();
+
+            account.DepositMoney(1500, DateTime.Now, "Stipend");
+
+
+
+        }
+
+
+        private void DMoneyButton_Clicked(object sender, EventArgs e)
+        {
+            if (EnterDepositAmount.Text != null)
+            {
+                decimal amount = Decimal.Parse(EnterDepositAmount.Text);
+                string description = EnterDepositDescription.Text.ToString();
+                _account.DepositMoney(amount, DateTime.Now, description);
+            }
+
+
+        }
+
+        private void WMoneyButton_Clicked(object sender, EventArgs e)
+        {
+            if (EnterWithdrawAmount.Text != null)
+            {
+                decimal amount = Decimal.Parse(EnterWithdrawAmount.Text);
+                string description = EnterWithdrawDescription.Text.ToString();
+                _account.WithdrawMoney(amount, DateTime.Now, description);
+            }
+        }
+
+        private void THistoryButton_Clicked(object sender, EventArgs e)
+        {
+            string history = _account.GetTransactionHistory();
+            DisplayAlert("Transaction History", history, "Press Enter to exit");
+
+        }
+    }
+
+}
